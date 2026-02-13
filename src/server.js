91cf523e0dc477acfce16ebac38a884204d547bd
nattakamon <nattakamon04@gmail.com>
2026-02-13 19:46:39 +0700

    Fix: final update for password reset and layout

--- src/server.js
+++ src/server.js
@@ -29,9 +29,7 @@ const __dirname = path.dirname(__filename);
 // -------------------------------------------------------------
 const allowedOrigins = [
     'https://skinherbcareweb2.netlify.app',
-    'https://skinherbcareweb1.onrender.com',
-    'http://localhost:3000',
-    'http://127.0.0.1:5500'
+    'https://skinherbcareweb1.onrender.com'
 ];
 
 const corsOptions = {
